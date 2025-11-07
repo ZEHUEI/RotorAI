f26021ad38fe2786ef7dc96002b2f89d297d473d
ZE HUEI LIM <zehueilim2005@gmail.com>
2025-11-07 12:39:46 +0000

    testing gitignore

--- Phase1/main.py
+++ Phase1/main.py
@@ -1,4 +1,16 @@
 #train with tensorflow and keras
 import tensorflow as tf
+import json
+import os
+import numpy as np
+from tensorflow.keras import layers, models
 
-print(tf.__version__)
+
+#COCO files
+train_img="Data/images/train"
+val_img="Data/images/validation"
+train_json = "Data/annotations/train"
+val_json = "Data/annotations/validation"
+
+
+#cross validation, train_test_split
