ddbf8bff9324abb8f64edaef2c77b32ce15fc70e
ZE HUEI LIM <zehueilim2005@gmail.com>
2026-02-19 20:07:44 +0000

    Update page.jsx

--- Frontend/rotorai/app/page.jsx
+++ Frontend/rotorai/app/page.jsx
@@ -1,8 +1,10 @@
 import Main from '@/Pages/Main';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   return (
     <main className="relative px-1.5 md:px-20 lg:px-40 text-[#D3D1CE]">
+      <Analytics />
       <Main />
     </main>
   );
