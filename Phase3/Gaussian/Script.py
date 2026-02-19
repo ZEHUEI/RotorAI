42167a7d304e9518ed7aedaeeacaef24a55e3e3f
ZE HUEI LIM <zehueilim2005@gmail.com>
2026-02-19 19:40:15 +0000

    idky

---
+++ Phase3/Gaussian/Script.py
@@ -0,0 +1,8 @@
+# Visual SLAM (ORB-SLAM / DROID-SLAM)
+# Pose estimation per frame
+# Online Gaussian optimization
+
+"""
+Camera-> SLAM-> YOLO detection-> Gaussian Model-> onLine optimization-> splat render
+All in Ubuntu
+"""
