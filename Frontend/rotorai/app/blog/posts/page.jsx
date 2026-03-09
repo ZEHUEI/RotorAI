9f62a88fafc07a83887b77f68bdd648c8c99f2fc
ZE HUEI LIM <zehueilim2005@gmail.com>
2026-03-09 17:33:07 +0000

    err

--- Frontend/rotorai/app/blog/posts/page.jsx
+++ Frontend/rotorai/app/blog/posts/page.jsx
@@ -1,9 +1,13 @@
+'use client';
+import { Suspense } from 'react';
 import BlogPosts from '@/Pages/Posts';
 
 export default function BlogPage() {
   return (
     <main className="relative px-1.5 md:px-20 lg:px-40 text-[#D3D1CE]">
-      <BlogPosts />
+      <Suspense fallback={<div>Loading...</div>}>
+        <BlogPostsContent />
+      </Suspense>
     </main>
   );
 }
