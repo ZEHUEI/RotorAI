c206996975e291ec3417fc7f8cbbc3b3d2ecb05d
ZE HUEI LIM <zehueilim2005@gmail.com>
2026-04-24 16:55:47 +0100

    match

--- backend/AIController.py
+++ backend/AIController.py
@@ -167,13 +167,13 @@ def image_to_base64(img_np):
 def apply_clahe(img_np):
     lab = cv2.cvtColor(img_np, cv2.COLOR_RGB2LAB)
     l, a, b = cv2.split(lab)
-    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+    clahe = cv2.createCLAHE(clipLimit=1.5, tileGridSize=(8, 8))
     cl = clahe.apply(l)
     limg = cv2.merge((cl, a, b))
     return cv2.cvtColor(limg, cv2.COLOR_LAB2RGB)
 
 
-TARGET_SIZE = (512, 512)
+TARGET_SIZE = (768, 768)
 BACKBONE = "resnet50"
 THRESHOLD = 0.65
 
