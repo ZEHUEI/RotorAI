508e11fcddc4a85cc29930910270c6e7ee63f32d
ZE HUEI LIM <zehueilim2005@gmail.com>
2026-04-26 15:19:11 +0100

    我爱你，你爱我

--- Frontend/rotorai/Pages/Gaussian.jsx
+++ Frontend/rotorai/Pages/Gaussian.jsx
@@ -32,8 +32,37 @@ const Gaussian = () => {
   }, []);
 
   return (
-    <div className="flex flex-col overflow-hidden items-center justify-center min-h-screen bg-black p-4">
+    <div className="relative flex flex-col overflow-hidden items-center justify-center min-h-screen bg-black p-4">
       {/* THE BOX */}
+
+      {/*controls */}
+      <div className="absolute top-4 left-1/2 z-30 -translate-x-1/2 text-white font-mono text-xs">
+        <div className="grid grid-cols-4 gap-6 bg-black/40 backdrop-blur-sm p-3 rounded-lg border border-white/10">
+          {/* Column 1 */}
+          <div className="flex flex-col items-center">
+            <span className="text-white/40 mb-1">Move</span>
+            <span className="tracking-widest">W A S D</span>
+          </div>
+
+          {/* Column 2 */}
+          <div className="flex flex-col items-center">
+            <span className="text-white/40 mb-1">Look</span>
+            <span>Right Click</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <span className="text-white/40 mb-1">Rotate</span>
+            <span>Left Click</span>
+          </div>
+
+          {/* Column 3 */}
+          <div className="flex flex-col items-center">
+            <span className="text-white/40 mb-1">Zoom</span>
+            <span>Scroll</span>
+          </div>
+        </div>
+      </div>
+
       <div className="relative w-full max-w-[1200px] aspect-video rounded-xl overflow-hidden shadow-2xl shadow-white/10 ring-2 ring-white/10 bg-[#050505]">
         {/* THE CONTAINER: Notice 'overflow-hidden' and 'h-full' */}
         <div
